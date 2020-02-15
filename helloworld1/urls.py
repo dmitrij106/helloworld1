bf484d6d005d40c24f50eee511eea99a521bf3a4
Dmitrij Shero <dmitrij106@yandex.ru>
2020-02-15 14:20:27 +0000

    version 1.0

--- helloworld1/urls.py
+++ helloworld1/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('app1.urls')),
     path('app1', include('app1.urls')),
     path('app1/', include('app1.urls')),
 ]
